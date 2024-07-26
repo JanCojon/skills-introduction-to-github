5afa4331774865e3c174f8e6e33394d1e9f88dad
JanCojon <ltenma90@gmail.com>
2024-07-26 21:28:30 +0100

    Create Prova.py

---
+++ Prova.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun May  5 12:21:03 2024
+
+@author: Lorenzo
+"""
+
+import streamlit as st
+import pandas as pd
+
+st.write("Here's our first attempt at using data to create a table:")
+st.write(pd.DataFrame({
+    'first column': [1, 2, 3, 4],
+    'second column': [10, 20, 30, 40]
+}))
+
